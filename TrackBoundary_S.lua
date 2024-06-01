65ae11fecfbf4aa796ec668ff6a89bb7350b8b3c
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2024-06-01 18:20:07 +0900

    Take maximum size of image into account.

--- TrackBoundary_S.lua
+++ TrackBoundary_S.lua
@@ -24,7 +24,7 @@ https://mit-license.org/
 ]]
 
 --
--- VERSION: v1.20-beta2
+-- VERSION: v1.20-beta3
 --
 
 --------------------------------
@@ -774,14 +774,21 @@ local function apply_chroma_key(col, range_chr,range_sat,adjust_bd,adjust_col,ad
 	range_sat = math_min(math_max(range_sat, 0), 256);
 	adjust_bd = math_min(math_max(adjust_bd, 0), 5);
 
+	-- take maximum size of image into account.
+	local anti_adjust_bd_sz = 0;
+	if anti_adjust_bd then
+		local w,h = obj.getpixel(); local mx,my = obj.getinfo("image_max");
+		anti_adjust_bd_sz = math.floor(math_min(adjust_bd, (mx-w)/2, (my-h)/2));
+	end
+
 	-- apply chorma key.
-	if anti_adjust_bd and adjust_bd > 0 then
+	if anti_adjust_bd_sz > 0 then
 		obj.effect("領域拡張","左",adjust_bd,"上",adjust_bd,"右",adjust_bd,"下",adjust_bd,"塗りつぶし",1);
 	end
 	obj.effect("クロマキー", "色相範囲",range_chr, "彩度範囲",range_sat,
 		"境界補正",adjust_bd, "色彩補正",adjust_col and 1 or 0, "透過補正",adjust_alpha and 1 or 0,
 		"color_yc",encode_yc_from_rgb(col), "status",1);
-	if anti_adjust_bd and adjust_bd > 0 then
+	if anti_adjust_bd_sz > 0 then
 		obj.effect("クリッピング","左",adjust_bd,"上",adjust_bd,"右",adjust_bd,"下",adjust_bd);
 	end
 end
@@ -805,13 +812,20 @@ local function apply_color_key(col, range_luma,range_uv,adjust_bd, anti_adjust_b
 	range_uv = math_min(math_max(range_uv, 0), 4096);
 	adjust_bd = math_min(math_max(adjust_bd, 0), 5);
 
+	-- take maximum size of image into account.
+	local anti_adjust_bd_sz = 0;
+	if anti_adjust_bd then
+		local w,h = obj.getpixel(); local mx,my = obj.getinfo("image_max");
+		anti_adjust_bd_sz = math.floor(math_min(adjust_bd, (mx-w)/2, (my-h)/2));
+	end
+
 	-- apply color key.
-	if anti_adjust_bd and adjust_bd > 0 then
+	if anti_adjust_bd_sz > 0 then
 		obj.effect("領域拡張","左",adjust_bd,"上",adjust_bd,"右",adjust_bd,"下",adjust_bd,"塗りつぶし",1);
 	end
 	obj.effect("カラーキー","輝度範囲",range_luma,"色差範囲",range_uv,"境界補正",adjust_bd,
 		"color_yc",encode_yc_from_rgb(col),"status",1)
-	if anti_adjust_bd and adjust_bd > 0 then
+	if anti_adjust_bd_sz > 0 then
 		obj.effect("クリッピング","左",adjust_bd,"上",adjust_bd,"右",adjust_bd,"下",adjust_bd);
 	end
 end
